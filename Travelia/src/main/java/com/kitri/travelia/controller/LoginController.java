a0928e6e1b69372adc0b9133a1191d796bcd61bd
sy <sy@DESKTOP-CF93Q3U>
2016-08-02 12:56:30 +0900

    Controller

---
+++ Travelia/src/main/java/com/kitri/travelia/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.kitri.travelia.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+	/*
+	@RequestMapping(value="login.html", method=RequestMethod.POST)
+	public String login(Member memeber, MemberDAO memberDAO, HttpSession session){
+		Member member = MemberDAO.getMember(member);
+		if(member!=null){
+			session.setAttribute("memberName", member.getNick() );
+			return "board.html";
+		}
+		else return "main.html";
+	}
+	*/
+
+}

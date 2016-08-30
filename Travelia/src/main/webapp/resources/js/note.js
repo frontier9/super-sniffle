5d6b25ead1ca737386dd136e09af408440484130
dreadcoder <centumcity.last@gmail.com>
2016-08-31 01:27:30 +0900

    16.08.31 Master note.js dynamic image upload fixed

--- Travelia/src/main/webapp/resources/js/note.js
+++ Travelia/src/main/webapp/resources/js/note.js
@@ -11,8 +11,22 @@ var nt =["nt3", "nt4","nt5","nt6","nt7","nt8","nt9"];
 $("#note_add").click(function(){
 
 	if(cnt_note<=6){
-	$(".note_wt").append('<div class="row wt"><div class="col-md-3"><img src="/resources/images/plus2.png" id="'+imgid[i]+'" style="width:150px; height:150px; cursor: pointer;" class="img-circle"></div><div class="col-md-9"><input type="text" name="'+title[i]+'" class="form-control title" placeholder="제목을 입력해 주세요."/><textarea class="form-control" rows="6"  style="width:494.25px;" placeholder="여행일지를 작성해 주세요." name="'+nt[i]+'" ></textarea></div><input type="file" id="'+intnm[i]+'" name="'+intn[i]+'" style="display: none;"/></div>');
-	$("#time_wt").append('<div class="row time_note"><div class="col-md-12"> <input type="text" class="form-control title" placeholder="제목을 입력해 주세요." name="'+time_title[i]+'"/><textarea class="form-control" rows="6"  style="width:470px;" placeholder="타임라인을 작성해 주세요." name="'+time[i]+'" ></textarea></div></div>');
+	$(".note_wt").append('<div class="row wt">' +
+        '<div class="col-md-3">' +
+        '<img src="/resources/images/plus2.png" id="'+imgid[i]+'" style="width:150px; height:150px; cursor: pointer;"' +
+        ' class="img-circle"></div><div class="col-md-9"><input type="text" name="'+title[i]+'"' +
+        ' class="form-control title" placeholder="제목을 입력해 주세요."/>' +
+        '<textarea class="form-control" rows="6"  style="width:494.25px;" placeholder="여행일지를 작성해 주세요." name="'+nt[i]+'" >' +
+        '</textarea>' +
+        '</div><input type="file" id="'+intnm[i]+'" name="'+intn[i]+'" style="display: none;"/></div>');
+
+	$("#time_wt").append('<div class="row time_note">' +
+        '<div class="col-md-12"> ' +
+        '<input type="text" class="form-control title" placeholder="제목을 입력해 주세요." name="'+time_title[i]+'"/>' +
+        '<textarea class="form-control" rows="6"  style="width:470px;" placeholder="타임라인을 작성해 주세요." name="'+time[i]+'" >' +
+        '</textarea>' +
+        '</div>' +
+        '</div>');
 	}
 
 	else{ alert('더 이상 작성 하실 수 없습니다.') };
@@ -52,40 +66,47 @@ filef.onchange = function(){
 $("img[id='nt_img1']")
 .css('cursor', 'pointer')
 .click(function() {
-    $("input[id='noteo']").click();
+    $("input[id='note1']").click();
 });
 
 
-var file = document.querySelector('#noteo');
+var file = document.querySelector('#note1');
 
 file.onchange = function () {
-    var fileList = file.files ;
+    var fileList = file.files;
 
     // 읽기
     var reader = new FileReader();
-    reader.readAsDataURL(fileList [0]);
+    reader.readAsDataURL(fileList[0]);
 
     //로드 한 후
-    reader.onload = function  () {
-        document.querySelector('#nt_img1').src = reader.result ;
+    reader.onload = function() {
+        document.querySelector('#nt_img1').src = reader.result;
     };
 };
+
+
+
+
+
+
+
 
 $("img[id='nt_img2']")
 .css('cursor', 'pointer')
 .click(function() {
-    $("input[id='notet']").click();
+    $("input[id='note2']").click();
 });
 
 
-var file1 = document.querySelector('#notet');
+var file1 = document.querySelector('#note2');
 
 file1.onchange = function () {
     var fileList1 = file1.files ;
 
     // 읽기
     var reader1 = new FileReader();
-    reader1.readAsDataURL(fileList1 [0]);
+    reader1.readAsDataURL(fileList1[0]);
 
     //로드 한 후
     reader1.onload = function  () {
@@ -93,21 +114,30 @@ file1.onchange = function () {
     };
 };
 
+
+
+
+
+
+
+
+
 $("img[id='nt_img3']")
 .css('cursor', 'pointer')
 .click(function() {
-    $("input[id='noteh']").click();
+    console.log("nt_img3 click");
+    $("input[id='note3']").click();
 });
 
 
-var file2 = document.querySelector('#noteh');
+var file2 = document.querySelector('#note3');
 
 file2.onchange = function () {
     var fileList2 = file2.files ;
 
     // 읽기
     var reader2 = new FileReader();
-    reader2.readAsDataURL(fileList2 [0]);
+    reader2.readAsDataURL(fileList2[0]);
 
     //로드 한 후
     reader2.onload = function  () {
@@ -115,30 +145,29 @@ file2.onchange = function () {
     };
 };
 
-
-var str = "test 입니다";
-
-$(document).on("click","#nt_img4",function(){
-		$("input[id='note4']").click();
-
-
+$(document).on('click', '#nt_img4', function() {
+    $('input[id="note4"]').click();
 });
 
 
-var file3 = document.getElementById('#note4');
+$('.note_wt').on('change', function(){
+    var file3 = document.getElementById('note4');
 
-file3.onchange = function () {
+    file3.onchange = function(){
+        var fileList3 = file3.files;
 
-	console.log(str);
+        var reader3 = new FileReader();
+        reader3.readAsDataURL(fileList3[0]);
+        reader3.onload = function(){
+            document.querySelector('#nt_img4').src = reader3.result;
+        }
+    }
+});
 
-    var fileList3 = file3.files ;
 
-    // 읽기
-    var reader3 = new FileReader();
-    reader3.readAsDataURL(fileList3 [0]);
 
-    //로드 한 후
-    reader3.onload = function  () {
-        document.querySelector('#nt_img4').src = reader3.result ;
-    };
-};
+
+
+
+
+

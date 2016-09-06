e52b53e7b04fb4fa5b0001f44250ff08873cc907
chang <wimo008700@gmail.com>
2016-09-06 17:50:57 +0900

    when scroll are moved , navbar is fadeout 09/06

--- Travelia/src/main/webapp/resources/js/trv_script.js
+++ Travelia/src/main/webapp/resources/js/trv_script.js
@@ -350,3 +350,22 @@ jQuery(function(){
         }
     });
 });
+
+
+$(function(){
+    var lastScrollTop = 0, delta = 30;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+       
+       if(Math.abs(lastScrollTop - st) <= delta)
+          return; // 스크롤값을 받아서 리턴한다.
+if ((st > lastScrollTop) && (lastScrollTop>0)) {
+      $("#trv_banner_menu ").css("top","-94px"); // 스크롤을 내렸을때 #header의 CSS 속성중 top 값을 -94px로 변경한다.
+  
+   } else {
+      $("#trv_banner_menu ").css("top","0px"); // 스크롤을 올렸을때 #header의 CSS 속성중 top 값을 0px로 변경한다.
+   }
+       lastScrollTop = st;
+    });
+});
+

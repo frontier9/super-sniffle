b771e0156b5e41f550021b15f5d522f45bfac14a
Owner <Owner@DESKTOP-I13OA8T>
2016-09-23 00:12:10 +0900

    Spring Security 권한 제어(TravelNote // Schedule )

--- Travelia/src/main/java/com/kitri/travelia/controller/TravelnoteController.java
+++ Travelia/src/main/java/com/kitri/travelia/controller/TravelnoteController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.kitri.travelia.domain.Travel_note;
 import com.kitri.travelia.service.TravelNoteService;
 
+
+@Secured({"ROLE_USER"})
 @Controller
 @RequestMapping(value="/travelnote/*")
 public class TravelnoteController {
